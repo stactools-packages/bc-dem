0d8c4c4b9b88f33124a675a0c2caa0db9cb6d352
Tyler Battle <tyler@battle.ca>
2021-11-09 13:36:21 -0800

    Rework create_collection and create_item

--- src/stactools/bc_dem/stac.py
+++ src/stactools/bc_dem/stac.py
@@ -1,11 +1,8 @@
 import logging
 import os
-from datetime import datetime, timezone
 from typing import Optional
 
 import fsspec
-import pystac
-import pytz
 import rasterio
 from pystac import (
     Asset,
@@ -13,20 +10,12 @@ from pystac import (
     Collection,
     Extent,
     Item,
-    Link,
     MediaType,
-    Provider,
-    ProviderRole,
     SpatialExtent,
     TemporalExtent,
 )
+from pystac.extensions.file import FileExtension
 from pystac.extensions.item_assets import AssetDefinition, ItemAssetsExtension
-from pystac.extensions.label import (
-    LabelClasses,
-    LabelExtension,
-    LabelTask,
-    LabelType,
-)
 from pystac.extensions.projection import ProjectionExtension
 from pystac.extensions.raster import (
     DataType,
@@ -34,26 +23,25 @@ from pystac.extensions.raster import (
     RasterExtension,
     Sampling,
 )
-from pystac.extensions.version import VersionExtension
 from shapely.geometry import box, mapping
 from stactools.core.io import ReadHrefModifier
 
 from stactools.bc_dem.constants import (
-    BCDEM_CRS,
-    BCDEM_EPSG,
-    BCDEM_ID,
-    DEM_PROVIDER,
+    COLLECTION_ID,
+    CRS_WKT,
     DESCRIPTION,
+    EPSG,
     LICENSE,
     LICENSE_LINK,
+    NO_DATA,
+    PROVIDERS,
     SPATIAL_EXTENT,
+    SPATIAL_RESOLUTION,
     START_DATE,
     TEMPORAL_EXTENT,
     THUMBNAIL_HREF,
     TITLE,
-    BCDEM_Resolution,
-    DEM_link,
-    Unit,
+    UNIT,
 )
 
 logger = logging.getLogger(__name__)
@@ -61,37 +49,33 @@ logger = logging.getLogger(__name__)
 
 def create_collection() -> Collection:
 
-    providers = [
-        Provider(
-            name="Province of British Columbia",
-            roles=[
-                ProviderRole.PRODUCER, ProviderRole.PROCESSOR,
-                ProviderRole.HOST
-            ],
-            url=
-            "https://governmentofbc.maps.arcgis.com/apps/MapSeries/index.html?appid=d06b37979b0c4709b7fcf2a1ed458e03",
-        )
-    ]
-
-    extent = Extent(
-        SpatialExtent([SPATIAL_EXTENT]),
-        TemporalExtent(TEMPORAL_EXTENT),
-    )
-
     collection = Collection(
-        id=BCDEM_ID,
+        id=COLLECTION_ID,
         title=TITLE,
         description=DESCRIPTION,
         license=LICENSE,
-        providers=[DEM_PROVIDER],
-        extent=extent,
+        providers=PROVIDERS,
+        extent=Extent(
+            SpatialExtent([SPATIAL_EXTENT]),
+            TemporalExtent(TEMPORAL_EXTENT),
+        ),
         catalog_type=CatalogType.RELATIVE_PUBLISHED,
     )
-    # version extension
-    collection_version = VersionExtension.ext(collection, add_if_missing=True)
+    collection.add_link(LICENSE_LINK)
+
+    collection.add_asset(
+        "thumbnail",
+        Asset(
+            href=THUMBNAIL_HREF,
+            media_type=MediaType.JPEG,
+            roles=["thumbnail"],
+            title="DEM Thumbnail",
+        ),
+    )
+
     collection_proj = ProjectionExtension.summaries(collection,
                                                     add_if_missing=True)
-    collection_proj.epsg = BCDEM_EPSG
+    collection_proj.epsg = [EPSG]
 
     collection_item_assets = ItemAssetsExtension.ext(collection,
                                                      add_if_missing=True)
@@ -113,55 +97,83 @@ def create_collection() -> Collection:
                                   data_type=DataType.UINT8).to_dict()
             ],
             "proj:epsg":
-            collection_proj.epsg
+            collection_proj.epsg,
         }),
     }
-
-    collection.add_link(LICENSE_LINK)
-
     return collection
 
 
 def create_item(
-        cog_href: str,
-        cog_read_href_modifier: Optional[ReadHrefModifier] = None) -> Item:
+    cog_href: str,
+    cog_read_href_modifier: Optional[ReadHrefModifier] = None,
+) -> Item:
 
     if cog_read_href_modifier:
-        modified_href = cog_read_href_modifier(cog_href)
+        cog_access_href = cog_read_href_modifier(cog_href)
     else:
-        modified_href = cog_href
+        cog_access_href = cog_href
 
-    with rasterio.open(modified_href) as dataset:
+    with rasterio.open(cog_access_href) as dataset:
         bbox = list(dataset.bounds)
         geometry = mapping(box(*bbox))
         transform = dataset.transform
         shape = dataset.shape
 
-    item = Item(id=os.path.splitext(os.path.basename(cog_href))[0],
-                geometry=geometry,
-                bbox=bbox,
-                datetime=START_DATE,
-                properties={},
-                stac_extensions={})
+    # TODO: Get the year of the tile. From the filename?
+    item = Item(
+        id=os.path.splitext(os.path.basename(cog_href))[0],
+        geometry=geometry,
+        bbox=bbox,
+        datetime=START_DATE,
+        properties={},
+        stac_extensions=[],
+    )
 
-    item.common_metadata.resolution = BCDEM_Resolution
-    item.common_metadata.Unit = Unit
-    item.common_metadata.providers = [DEM_PROVIDER]
+    item.common_metadata.resolution = SPATIAL_RESOLUTION
+    item.common_metadata.unit = UNIT
+    item.common_metadata.providers = PROVIDERS
     item.common_metadata.license = "proprietary"
 
     parts = os.path.basename(cog_href).split("_")
     title = parts[1]
-    item.add_asset(
-        "data",
-        Asset(href=cog_href,
-              title=title,
-              description=None,
-              media_type=MediaType.COG,
-              roles=["data"]))
 
-    projection = ProjectionExtension.ext(item, add_if_missing=True)
-    projection.epsg = BCDEM_EPSG
-    projection.transform = transform[0:6]
-    projection.shape = shape
+    item_projection = ProjectionExtension.ext(item, add_if_missing=True)
+    item_projection.epsg = EPSG
+    item_projection.wkt2 = CRS_WKT
+    item_projection.transform = transform
+    item_projection.shape = shape
+
+    cog_asset = Asset(
+        href=cog_href,
+        title=title,
+        description=None,
+        media_type=MediaType.COG,
+        roles=["data"],
+    )
+    item.add_asset("data", cog_asset)
+
+    cog_asset_file = FileExtension.ext(cog_asset, add_if_missing=True)
+    with fsspec.open(cog_access_href) as file:
+        size = file.size
+        if size is not None:
+            cog_asset_file.size = size
+
+    cog_asset_raster = RasterExtension.ext(cog_asset, add_if_missing=True)
+    cog_asset_raster.bands = [
+        RasterBand.create(
+            nodata=NO_DATA,
+            sampling=Sampling.AREA,
+            data_type=DataType.FLOAT32,
+            spatial_resolution=SPATIAL_RESOLUTION,
+        )
+    ]
+
+    cog_asset_projection = ProjectionExtension.ext(cog_asset,
+                                                   add_if_missing=True)
+    cog_asset_projection.epsg = item_projection.epsg
+    cog_asset_projection.wkt2 = item_projection.wkt2
+    cog_asset_projection.bbox = item_projection.bbox
+    cog_asset_projection.transform = item_projection.transform
+    cog_asset_projection.shape = item_projection.shape
 
     return item
