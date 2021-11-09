1e5426b69d04860d0fe0bd4f9bb877d3ca824329
Tyler Battle <tyler@battle.ca>
2021-11-09 13:35:28 -0800

    Cleanup constants

--- src/stactools/bc_dem/constants.py
+++ src/stactools/bc_dem/constants.py
@@ -1,28 +1,38 @@
+# flake8: noqa
+
 from datetime import datetime
 
 from pyproj import CRS
 from pystac import Link, Provider, ProviderRole
 
-BCDEM_ID = "BC_DEM"
-BCDEM_EPSG = 3157
-BCDEM_Resolution = 1
-Unit = "meters"
-BCDEM_CRS = CRS.from_epsg(BCDEM_EPSG)
+COLLECTION_ID = "bc-dem"
+EPSG = 3157
+CRS_WKT = CRS.from_epsg(EPSG).to_wkt()
+SPATIAL_RESOLUTION = 1
+UNIT = "meters"
 LICENSE = "proprietary"
-DEM_link = "https://www2.gov.bc.ca/gov/content/data/open-data/open-government-licence-bc"
+LICENSE_URL = "https://www2.gov.bc.ca/gov/content/data/open-data/open-government-licence-bc"
 LICENSE_LINK = Link(rel="license",
-                    target=DEM_link,
+                    target=LICENSE_URL,
                     title="Open Government Licence - British Columbia")
-SPATIAL_EXTENT = []
+SPATIAL_EXTENT = []  # TODO: Figure out the total extent of the collection
 START_DATE = datetime(2016, 1, 1)
-TEMPORAL_EXTENT = [datetime(2016, 1, 1) or None, datetime(2019, 12, 31)]
-THUMBNAIL_HREF = ""
+TEMPORAL_EXTENT = [START_DATE or None, datetime(2019, 12, 31)]
+THUMBNAIL_HREF = "https://www.arcgis.com/sharing/rest/content/items/c3bf9d29cc7b44718dca370435353994/resources/ex1_dem__1625007392868.JPG"
 DESCRIPTION = """LidarBC's Open LiDAR Data Portal is an initiative to provide open public access to LiDAR and associated datasets collected by the Province of British Columbia. The data presented herein is released as Open Data under the Open Government Licence – British Columbia (OGL-BC).
 Digital Elevation Model (DEM) is a derivative product of LiDAR and a representation of the terrain bare earth surface. The bare earth DEM is developed by removing vegetation and structures from the LiDAR data and developed through interpolation of the elevation data."""
 TITLE = 'LidarBC'
-DEM_PROVIDER = Provider(
-    name="Province of British Columbia",
-    roles=[ProviderRole.PRODUCER, ProviderRole.PROCESSOR, ProviderRole.HOST],
-    url=
-    "https://governmentofbc.maps.arcgis.com/apps/MapSeries/index.html?appid=d06b37979b0c4709b7fcf2a1ed458e03"
-)
+PROVIDERS = [
+    Provider(
+        name="Province of British Columbia",
+        roles=[
+            ProviderRole.HOST,
+            ProviderRole.PROCESSOR,
+            ProviderRole.PRODUCER,
+        ],
+        url=
+        "https://governmentofbc.maps.arcgis.com/apps/MapSeries/index.html?appid=d06b37979b0c4709b7fcf2a1ed458e03",
+    )
+]
+
+NO_DATA = -9999
